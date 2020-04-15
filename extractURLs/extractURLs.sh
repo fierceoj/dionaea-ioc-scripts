18c893e76351783978d7568baee9d54063d8f5f1
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-04-15 15:51:16 -0500

    Update extractURLs.sh

--- extractURLs/extractURLs.sh
+++ extractURLs/extractURLs.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+print_urls () {
+        for i in $1
+                do
+                        echo "FILE: $i"
+                        sudo strings $i | grep -Eo "https?:\/\/\S+" 
+                        echo -e '\n'
+                done
+}
+
+if [ $1 == "--all" ] 
+then 
+        print_urls "*"
+                
+else
+        print_urls ""$@""
+fi 
