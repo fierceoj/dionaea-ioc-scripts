ee7a4069436725643b1bbbd5074bc6f05b9c6005
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-04-16 13:02:11 -0500

    Update extractIPs.sh

--- extractIPs/extractIPs.sh
+++ extractIPs/extractIPs.sh
@@ -4,22 +4,28 @@
 filename=$(basename $(pwd))  
 filename+="_IPs.txt"
 
-#extract ftp scanner IPs
-echo "FTP Port 21 Scanners:" | tee -a ~/$filename 
-ls | grep "ftpd" | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u | tee -a ~/$filename
-echo -e '\n' | tee -a ~/$filename
+#function to extract and print scanner IPs from bistream logs
+print_IPs () {
+        for i in $@ 
+                do
+                        echo "Service:" | tee -a ~/$filename
+                        echo $i | tee -a ~/$filename
+                        echo "Port:" | tee -a ~/$filename
+                        ls | grep $i | grep -oE "\-[0-9]{2,4}\-" | sort -u | tr -d - | tee -a ~/$filename
+                        echo "Scanner IPs:" | tee -a ~/$filename
+                        ls | grep $i | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u | tee -a ~/$filename
+                        echo -e '\n' | tee -a ~/$filename
+                done
+}
 
-#extract mssql scanner IPs
-echo "MSSQL Port 1433 Scanners:" | tee -a ~/$filename
-ls | grep "mssqld" | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u | tee -a ~/$filename
-echo -e '\n' | tee -a ~/$filename
+#either check all protocol/services that were logged or check only specific protocols           
+if [ $1 == "--all" ] 
+then 
+        services=$(ls | grep -oE "[a-zA-Z]{4,15}\-" | sort -u)
+        services=$(echo $services | tr -d -)
+        services=($services)
+        print_IPs ${services[@]}
+else
+        print_IPs $@    
 
-#extract mysql scanner IPs
-echo "MySQL Port 3306 Scanners:" | tee -a ~/$filename
-ls | grep "mysqld" | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u | tee -a ~/$filename
-echo  -e '\n' | tee -a ~/$filename
-
-#extract smb scanner IPs
-echo "SMB Port 445 Scanners:" | tee -a ~/$filename
-ls | grep "smbd" | grep -oE "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u | tee -a ~/$filename
-echo -e '\n' | tee -a ~/$filename
+fi
