13dbcc31a90e2360ade30c01ce41da1b48051fcd
fierceoj <61847904+fierceoj@users.noreply.github.com>
2020-04-17 10:55:14 -0500

    Update extractURLs.sh

--- extractURLs/extractURLs.sh
+++ extractURLs/extractURLs.sh
@@ -15,7 +15,7 @@ print_urls () {
 #change directory to the binaries folder
 cd /opt/dionaea/var/dionaea/binaries
 
-#either check all binares or just binary names provided
+#either check all binares or just binary names provided in arguments
 if [ $1 == "--all" ] 
 then 
         print_urls "*"
